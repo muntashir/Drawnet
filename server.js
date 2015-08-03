ebd21000a3052b7a65ab847a65c5badac6f72d7a
Muntashir <kazimuntashir@gmail.com>
2015-08-03 09:48:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var io = require('socket.io')(server);
 //});
 
 var synaptic = require('synaptic');
-net = new synaptic.Architect.Perceptron(80 * 80, 10, 36);
+net = new synaptic.Architect.Perceptron(80 * 80, 15, 36);
 
 //Init socket
 io.on('connection', function (socket) {
